799287d566e2068e260d802175430d0ab81b8e6f
MarvelTiter <outlinmt@outlook.com>
2022-04-08 15:22:32 +0800

    queryresult 帮助方法

--- Project.Models/QueryResult.cs
+++ Project.Models/QueryResult.cs
@@ -11,5 +11,38 @@ namespace Project.Models
         public bool Success { get; set; } = false;
         public string Message { get; set; } = "Error";
         public T Payload { get; set; }
+
+        public static QueryResult<T> SuccessResult(T payload, string msg = "")
+        {
+            return new QueryResult<T>()
+            {
+                Success = true,
+                Message = msg,
+                Payload = payload,
+            };
+        }
+        public static QueryResult<T> FailResult(T payload, string msg = "")
+        {
+            return new QueryResult<T>()
+            {
+                Success = false,
+                Message = msg,
+                Payload = payload,
+            };
+        }
+
+        public static QueryResult<PagingResult<T>> PagingResult(IEnumerable<T> payload, int total)
+        {
+            return new QueryResult<PagingResult<T>>
+            {
+                Success = true,
+                Message = "",
+                Payload = new PagingResult<T>
+                {
+                    TotalRecord = total,
+                    Data = payload
+                }
+            };
+        }
     }
 }
