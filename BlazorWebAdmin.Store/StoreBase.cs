94d934b9b7b7f765cf79e97f65b75e4be29cc7b2
MarvelTiter <outlinmt@outlook.com>
2022-04-11 11:05:23 +0800

    store

--- BlazorWebAdmin.Store/StoreBase.cs
+++ BlazorWebAdmin.Store/StoreBase.cs
@@ -2,6 +2,23 @@
 {
     public abstract class StoreBase
     {
+        private Dictionary<string, object> baseStore;
+
+        public void Commit<T>(string key, object v) where T : Type
+        {
+            var fullName = $"{typeof(T).FullName}_{key}";
+            baseStore.TryAdd(fullName, v);
+        }
+
+        public T Get<T>(string key) where T:Type
+        {
+            if(baseStore.TryGetValue(key, out var v))
+            {
+                return (T)v;
+            }
+            return default;
+        }
+
         public event Action DataChangedEvent;
         protected void NotifyChanged()
         {
