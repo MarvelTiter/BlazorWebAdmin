fc2e0cd3aa59a67d353187390e0e9b985173c4e0
MarvelTiter <outlinmt@outlook.com>
2022-04-11 11:17:00 +0800

    删除store数据缓存

--- BlazorWebAdmin.Store/StoreBase.cs
+++ BlazorWebAdmin.Store/StoreBase.cs
@@ -1,24 +1,7 @@
 ﻿namespace BlazorWebAdmin.Store
 {
     public abstract class StoreBase
-    {
-        private Dictionary<string, object> baseStore;
-
-        public void Commit<T>(string key, object v)
-        {
-            var fullName = $"{typeof(T).FullName}_{key}";
-            baseStore.TryAdd(fullName, v);
-        }
-
-        public object Get<T>(string key)
-        {
-            if(baseStore.TryGetValue(key, out var v))
-            {
-                return v;
-            }
-            return default;
-        }
-
+    {        
         public event Action DataChangedEvent;
         protected void NotifyChanged()
         {
