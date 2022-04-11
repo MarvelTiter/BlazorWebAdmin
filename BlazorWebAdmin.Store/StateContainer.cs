5772936d3dd2aa0af39744bb3a5fffcb6ed0f010
MarvelTiter <outlinmt@outlook.com>
2022-04-11 11:58:15 +0800

    state

---
+++ BlazorWebAdmin.Store/StateContainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWebAdmin.Store
+{
+    public class StateContainer
+    {
+        private Dictionary<string, object> _state = new();
+        public T Get<O, T>(string key) where T : class, new()
+        {
+            var fullKey = $"{typeof(O).FullName}_{key}";
+            if (!_state.TryGetValue(fullKey, out var value))            
+            {
+                value = new T();
+                _state.Add(fullKey, value);
+            }
+            return (T)value;
+        }
+    }
+}
