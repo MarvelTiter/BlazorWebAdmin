f45933ac91602de1f5bd0bcf7fbd133aea16e87f
marvel <outlinmt@outlook.com>
2022-04-11 22:19:52 +0800

    CustomSelect组件

--- BlazorWebAdmin.Store/CounterStore.cs
+++ BlazorWebAdmin.Store/CounterStore.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,20 @@ namespace BlazorWebAdmin.Store
     public class CounterStore : StoreBase
     {
         public int Count { get; set; }
+        public SelectItem<string> Items { get; set; }
+        public CounterStore()
+        {
+            Init();
+        }
+
+        private async void Init()
+        {
+            await Task.Delay(1000);
+            Items = new SelectItem<string>();
+            Items.Add("k1", "k1");
+            Items.Add("k2", "k2");
+            Items.Add("k3", "k3");
+            Items.Add("k4", "k4");
+        }
     }
 }
