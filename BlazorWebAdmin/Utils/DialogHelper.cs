9868aebca337ebf3376095ca23eb6587a53ac9bc
MarvelTiter <outlinmt@outlook.com>
2022-04-14 17:06:38 +0800

    DialogHelper

---
+++ BlazorWebAdmin/Utils/DialogHelper.cs
@@ -0,0 +1,27 @@
+﻿using AntDesign;
+
+namespace BlazorWebAdmin.Utils
+{
+    public static class DialogHelper
+    {
+        public static async Task<T> OpenDialog<Template, T>(this ModalService service, ModalOptions options, T param) where Template : FeedbackComponent<T, T>
+        {
+            TaskCompletionSource<T> tcs = new();
+            var modalRef = await service.CreateModalAsync<Template, T, T>(options, param);
+
+            modalRef.OnOk = async (result) =>
+            {
+                await Task.Delay(1);
+                tcs.SetResult(result);
+            };
+
+            modalRef.OnCancel = async (result) =>
+            {
+                await Task.Delay(1);
+                tcs.SetCanceled();
+            };
+
+            return await tcs.Task;
+        }
+    }
+}
