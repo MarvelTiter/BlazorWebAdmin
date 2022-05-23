a06c466734986160ad25ee1c120556721dd14f64
marvel <outlinmt@outlook.com>
2022-05-23 23:12:11 +0800

    Aop测试用例 CodeGenTest : IForTest

--- Project.AppCore/Services/IForTest.cs
+++ Project.AppCore/Services/IForTest.cs
@@ -8,14 +8,20 @@ using System.Threading.Tasks;
 
 namespace Project.AppCore.Services
 {
-    //[Aspectable(AspectHandleType =typeof(LogAop))]
+    [Aspectable(AspectHandleType = typeof(LogAop))]
     public interface IForTest
     {
+        [LogInfo(Action = "有Async,返回Task<T>", Module = "测试")]
         Task<int> ReturnTaskValueAsync();
+        [LogInfo(Action = "无Async,返回Task<T>", Module = "测试")]
         Task<int> ReturnTaskValue();
+        [LogInfo(Action = "有Async,返回Task", Module = "测试")]
         Task ReturnTaskAsync();
+        [LogInfo(Action = "无Async,返回Task", Module = "测试")]
         Task ReturnTask();
+        [LogInfo(Action = "同步无返回", Module = "测试")]
         void ReturnVoid();
+        [LogInfo(Action = "异步有返回", Module = "测试")]
         int ReturnInt();
     }
 }
