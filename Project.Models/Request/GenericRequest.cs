9507a7724c9ba9c176a252248f5502858439c57e
Marvel YAO <outlinmt@outlook.com>
2023-01-06 11:51:57 +0800

    修改GenericRequest默认参数

--- Project.Models/Request/GenericRequest.cs
+++ Project.Models/Request/GenericRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MT.KitTools.DateTimeExtension;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -13,20 +14,30 @@ namespace Project.Models.Request
         public int PageSize { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
+        /// <summary>
+        /// <para>是否格式化日期 默认 true</para>
+        /// <para>StartTime => yyyy-MM-dd 00:00:00</para>
+        /// <para>EndTime => yyyy-MM-dd 23:59:59</para>
+        /// </summary>
+        public bool FixTime { get; set; }
         public Expression? Expression { get; set; }
     }
     public interface IRequest<T> : IRequest
     {
         public new Expression<Func<T, bool>>? Expression { get; set; }
     }
-    
+
     public class GenericRequest<T> : IRequest<T>
     {
         public string Keyword { get; set; }
         public int PageIndex { get; set; } = 1;
         public int PageSize { get; set; } = 10;
-        public DateTime StartTime { get; set; } = DateTime.Now;
-        public DateTime EndTime { get; set; } = DateTime.Now;
+        public bool FixTime { get; set; } = true;
+
+        private DateTime _start = DateTime.Now;
+        public DateTime StartTime { get => FixTime ? _start.DayStart() : _start; set => _start = value; }
+        private DateTime _end = DateTime.Now;
+        public DateTime EndTime { get => FixTime ? _end.DayEnd() : _end; set => _end = value; }
         public Expression<Func<T, bool>>? Expression { get; set; }
         public int From => (PageIndex - 1) * PageSize;
         public int To => PageIndex * PageSize;
