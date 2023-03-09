0ca4922d793dbd927c2bba4b927593b1baf6a7ae
Marvel YAO <outlinmt@outlook.com>
2023-03-09 16:18:29 +0800

    提取登录校验函数，方便其他项目修改

---
+++ Project.Services/LoginService.UserInfo.cs
@@ -0,0 +1,55 @@
+﻿using DocumentFormat.OpenXml.EMMA;
+using MDbContext.Repository;
+using Project.Models;
+using Project.Models.Entities;
+using Project.Models.Permissions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Services
+{
+	public partial class LoginService
+	{
+		public async Task<IQueryResult<UserInfo>> GetUserInfo(string username, string password)
+		{
+			var u = await context.Repository<User>().GetSingleAsync(u => u.UserId == username);
+			var userInfo = new UserInfo
+			{
+				UserId = username,
+				UserName = u?.UserName ?? "",
+			};
+			var result = userInfo.Result(u != null);
+			if (!result.Success)
+			{
+				result.Message = $"用户：{username} 不存在";
+				return result;
+			}
+			if (u!.Password != password)
+			{
+				result.Message = "密码错误";
+				result.Success = false;
+				return result;
+			}
+			var roles = await context.Repository<UserRole>().GetListAsync(ur => ur.UserId == username);
+			//var userInfo = new UserInfo
+			//{
+			//    UserId = username,
+			//    UserName = u.UserName,
+			//    Roles = roles.Select(ur => ur.RoleId).ToList()
+			//};
+			userInfo.Roles = roles.Select(ur => ur.RoleId).ToList();
+
+			return result;
+		}
+
+		public async Task<int> UpdateLoginInfo(UserInfo info)
+		{
+			return await context.Update<User>()
+									.Set(u => u.LastLogin, DateTime.Now)
+									.Where(u => u.UserId == info.UserId).ExecuteAsync();
+		}
+	}
+}
--- Project.Services/LoginService.cs
+++ Project.Services/LoginService.cs
@@ -8,65 +8,37 @@ using Project.Models.Permissions;
 
 namespace Project.Services
 {
-    public partial class LoginService : ILoginService
-    {
-        private readonly IExpressionContext context;
+	public partial class LoginService : ILoginService
+	{
+		private readonly IExpressionContext context;
 
-        public LoginService(IExpressionContext context)
-        {
-            this.context = context;
-        }
-        public Task<bool> CheckUser(UserInfo info)
-        {
-            return Task.FromResult(true);
-        }
-
-        public async Task<IQueryResult<UserInfo>> LoginAsync(string username, string password)
-        {
-            var u = await context.Repository<User>().GetSingleAsync(u => u.UserId == username);
-            var userInfo = new UserInfo
-            {
-                UserId = username,
-                UserName = u?.UserName ?? "",
-            };
-            var result = userInfo.Result(u != null);
-            if (!result.Success)
-            {
-                result.Message = $"用户：{username} 不存在";
-                return result;
-            }
-            if (u!.Password != password)
-            {
-                result.Message = "密码错误";
-                result.Success = false;
-                return result;
-            }
-            var roles = await context.Repository<UserRole>().GetListAsync(ur => ur.UserId == username);
-            //var userInfo = new UserInfo
-            //{
-            //    UserId = username,
-            //    UserName = u.UserName,
-            //    Roles = roles.Select(ur => ur.RoleId).ToList()
-            //};
-            userInfo.Roles = roles.Select(ur => ur.RoleId).ToList();
-            await UpdateLastLoginTimeAsync(userInfo);
-            //result.SetPayload(userInfo);
-            return result;
-        }
-
-        public async Task<IQueryResult<bool>> UpdateLastLoginTimeAsync(UserInfo info)
-        {
-            var flag = await context.Update<User>()
-                                    .Set(u => u.LastLogin, DateTime.Now)
-                                    .Where(u => u.UserId == info.UserId).ExecuteAsync();
-            info.Payload = JwtTokenHelper.GetToken(info.UserId, null, info.Roles.ToArray());
-            return (flag > 0).Result();
-        }
-
-        public Task<IQueryResult<bool>> LogoutAsync()
-        {
-            //TODO 用户登出处理
-            return Task.FromResult((true).Result());
+		public LoginService(IExpressionContext context)
+		{
+			this.context = context;
 		}
-    }
+		public Task<bool> CheckUser(UserInfo info)
+		{
+			return Task.FromResult(true);
+		}
+
+		public async Task<IQueryResult<UserInfo>> LoginAsync(string username, string password)
+		{
+			var result = await GetUserInfo(username, password);
+			await UpdateLastLoginTimeAsync(result.Payload);
+			return result;
+		}
+
+		public async Task<IQueryResult<bool>> UpdateLastLoginTimeAsync(UserInfo info)
+		{			
+			var flag = await UpdateLoginInfo(info);
+			info.Payload = JwtTokenHelper.GetToken(info.UserId, null, info.Roles.ToArray());
+			return (flag > 0).Result();
+		}
+
+		public Task<IQueryResult<bool>> LogoutAsync()
+		{
+			//TODO 用户登出处理
+			return Task.FromResult((true).Result());
+		}
+	}
 }
