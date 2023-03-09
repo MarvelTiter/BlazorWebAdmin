f787e1c2f37638a703ec8a2bf12cec38300e263d
Marvel YAO <outlinmt@outlook.com>
2023-03-09 16:39:27 +0800

    抽象User

--- Project.Models/Entities/User.cs
+++ Project.Models/Entities/User.cs
@@ -1,28 +1,29 @@
 ﻿using MDbEntity.Attributes;
 using Project.Common.Attributes;
+using Project.Models.Permissions;
 
 namespace Project.Models.Entities
 {
-    [Table(Name = "USER")]
-    public class User
-    {
-        [ColumnDefinition("用户ID", 0, fix: "left", width: "100px")]
-        [Column(Name = "USER_ID")]
-        public string UserId { get; set; }
-        [ColumnDefinition("用户名", 1, fix: "left", width: "100px")]
-        [Column(Name = "USER_NAME")]
-        public string UserName { get; set; }
-        [ColumnDefinition("用户密码", 2, fix: "left")]
-        [Column(Name = "PASSWORD")]
-        public string Password { get; set; }
-        [ColumnDefinition("年龄", 3)]
-        [Column(Name = "AGE")]
-        public int? Age { get; set; }
-        [ColumnDefinition("签名", 4)]
-        [Column(Name = "SIGN")]
-        public string Sign { get; set; }
-        [ColumnDefinition("最后登录", 5)]
-        [Column(Name = "LAST_LOGIN")]
-        public DateTime? LastLogin { get; set; }
-    }
+	[Table(Name = "USER")]
+	public class User : IUser
+	{
+		[ColumnDefinition("用户ID", 0, fix: "left", width: "100px")]
+		[Column(Name = "USER_ID")]
+		public string UserId { get; set; }
+		[ColumnDefinition("用户名", 1, fix: "left", width: "100px")]
+		[Column(Name = "USER_NAME")]
+		public string UserName { get; set; }
+		[ColumnDefinition("用户密码", 2, fix: "left")]
+		[Column(Name = "PASSWORD")]
+		public string Password { get; set; }
+		[ColumnDefinition("年龄", 3)]
+		[Column(Name = "AGE")]
+		public int? Age { get; set; }
+		[ColumnDefinition("签名", 4)]
+		[Column(Name = "SIGN")]
+		public string Sign { get; set; }
+		[ColumnDefinition("最后登录", 5)]
+		[Column(Name = "LAST_LOGIN")]
+		public DateTime? LastLogin { get; set; }
+	}
 }
---
+++ Project.Models/Permissions/IUser.cs
@@ -0,0 +1,8 @@
+﻿namespace Project.Models.Permissions
+{
+	public interface IUser
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
