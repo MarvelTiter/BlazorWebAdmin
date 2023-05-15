675e4cfbd880d64ce7756ade346c3e740dbda469
Marvel YAO <outlinmt@outlook.com>
2023-05-15 15:50:49 +0800

    登录页多语言，右上角NavMenu多语言

--- BlazorWebAdmin/Shared/Login.razor.cs
+++ BlazorWebAdmin/Shared/Login.razor.cs
@@ -3,6 +3,7 @@ using BlazorWeb.Shared.Layouts;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.Localization;
 using Project.AppCore.Auth;
 using Project.AppCore.Services;
 using Project.AppCore.Store;
@@ -20,6 +21,7 @@ namespace BlazorWebAdmin.Shared
         [Inject] public ILoginService LoginSrv { get; set; }
         [Inject] public AuthenticationStateProvider Auth { get; set; }
         [Inject] NavigationManager NavigationManager { get; set; }
+        [Inject] IStringLocalizer<Login> Localizer { get; set; }
         [CascadingParameter] RootLayout Root { get; set; }
         public bool Loading { get; set; } = false;
         public string? Redirect { get; set; }
@@ -53,7 +55,7 @@ namespace BlazorWebAdmin.Shared
                 UserStore.SetUser(result.Payload);
                 await ((CustomAuthenticationStateProvider)Auth).IdentifyUser(result.Payload);
                 await RouterStore.InitRoutersAsync(result.Payload);
-                _ = MessageSrv.Info("登录成功");
+                _ = MessageSrv.Info(Localizer["Login.SuccessTips"].Value);
                 Root.OnKeyDown -= OnPressEnter;
                 if (string.IsNullOrEmpty(Redirect))
                     NavigationManager.NavigateTo("/");
