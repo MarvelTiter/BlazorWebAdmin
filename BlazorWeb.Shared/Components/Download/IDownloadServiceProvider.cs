d3377b3fb7c79821cbcbae71f251c4ed315a7a2d
marvel <outlinmt@outlook.com>
2023-11-22 11:26:46 +0800

    添加JsTimer组件 添加IDownloadService，优化下载形式

---
+++ BlazorWeb.Shared/Components/Download/IDownloadServiceProvider.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorWeb.Shared
+{
+
+    public interface IDownloadServiceProvider
+    {
+        IDownloadService? GetService();
+        void Register(IDownloadService service);
+    }
+
+    public class DownloadServiceProvider : IDownloadServiceProvider
+    {
+        private IDownloadService? service;
+        public IDownloadService? GetService() => service;
+
+        public void Register(IDownloadService service)
+        {
+            this.service = service;
+        }
+    }
+}
--- BlazorWeb.Shared/Extensions/SharedComponents.cs
+++ BlazorWeb.Shared/Extensions/SharedComponents.cs
@@ -7,6 +7,12 @@ namespace BlazorWeb.Shared.Extensions
         public static IServiceCollection SharedComponentsInit(this IServiceCollection services)
         {
             services.AddScoped<IReconnectorProvider, ReconnectorProvider>();
+            services.AddScoped<IDownloadServiceProvider, DownloadServiceProvider>();
+            services.AddScoped<IDownloadService>(provider =>
+            {
+                var sp = provider.GetService<IDownloadServiceProvider>();
+                return sp?.GetService();
+            });
             return services;
         }
     }
