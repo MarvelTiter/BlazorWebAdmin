0035d0cbb74e2185c87cdd771e91c6df2abacad1
marvel <outlinmt@outlook.com>
2024-04-11 17:42:38 +0800

    缓存帮助类

--- src/Project.Constraints/Common/StaticCache.cs
+++ src/Project.Constraints/Common/StaticCache.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections.Concurrent;
 
-namespace Project.Constraints.Common
-{
-    public static class StaticCache<T>
-    {
-        static readonly ConcurrentDictionary<string, T> caches = new();
+namespace Project.Constraints.Common;
 
-        public static T GetOrAdd(string key, Func<T> func)
-        {
-            return caches.GetOrAdd(key, func.Invoke());
-        }
+public static class StaticCache<T>
+{
+    static readonly ConcurrentDictionary<string, T> caches = new();
+
+    public static T GetCache(string key)
+    {
+        if (caches.TryGetValue(key, out var cache)) { return cache; }
+        return default!;
+    }
+
+    public static void AddCache(string key, T value)
+    {
+        caches.GetOrAdd(key, value);
+    }
+
+    public static T GetOrAdd(string key, Func<T> func)
+    {
+        return caches.GetOrAdd(key, func.Invoke());
     }
 }
