f50a10eafae5ef9c11b54c7c764b9a582696b9ef
marvel <outlinmt@outlook.com>
2024-04-26 17:36:27 +0800

    备份代码

---
+++ src/Shared/MT.LightTask/IocExtension/SerivceCollectionExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT.LightTask
+{
+    public static class SerivceCollectionExtension
+    {
+        public static IServiceCollection AddLightTasks(this IServiceCollection services)
+        {
+            services.AddSingleton<TaskFactory>();
+            return services;
+        }
+    }
+}
