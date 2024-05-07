f3c1027997b8927df57876cad8c4bcc76f0877f6
marvel <outlinmt@outlook.com>
2024-05-07 10:58:27 +0800

    拓展queryresult

--- src/Shared/Project.Constraints/Models/QueryResult.cs
+++ src/Shared/Project.Constraints/Models/QueryResult.cs
@@ -1,15 +1,22 @@
-﻿namespace Project.Constraints.Models;
+﻿using System.Data;
+
+namespace Project.Constraints.Models;
 
 public interface IQueryResult
 {
     bool Success { get; set; }
     int Code { get; set; }
-    string Message { get; set; }
-    object Payload { get; set; }
+    string? Message { get; set; }
+    object? Payload { get; set; }
 }
 public interface IQueryResult<T> : IQueryResult
 {
-    new T Payload { get; set; }
+    new T? Payload { get; set; }
+}
+
+public interface IDataTableResult : IQueryResult<DataTable>
+{
+    int TotalRecord { get; set; }
 }
 
 public interface IQueryCollectionResult<T> : IQueryResult
@@ -22,9 +29,20 @@ public class QueryResult<T> : IQueryResult<T>
 {
     public bool Success { get; set; }
     public int Code { get; set; }
-    public string Message { get; set; }
-    public T Payload { get; set; }
-    object IQueryResult.Payload { get => Payload; set => Payload = (T)value; }
+    public string? Message { get; set; }
+    public T? Payload { get; set; }
+    object? IQueryResult.Payload { get => Payload; set => Payload = (T?)value; }
+}
+
+[IgnoreAutoInject]
+public class DataTableResult : IDataTableResult
+{
+    public bool Success { get; set; }
+    public int Code { get; set; }
+    public string? Message { get; set; }
+    public int TotalRecord { get; set; }
+    public DataTable? Payload { get; set; }
+    object? IQueryResult.Payload { get => Payload; set => Payload = value as DataTable; }
 }
 
 [IgnoreAutoInject]
@@ -32,15 +50,25 @@ public class QueryCollectionResult<T> : IQueryCollectionResult<T>
 {
     public bool Success { get; set; }
     public int Code { get; set; }
-    public string Message { get; set; }
+    public string? Message { get; set; }
     public int TotalRecord { get; set; }
-    public IEnumerable<T> Payload { get; set; }
-    object IQueryResult.Payload { get => Payload; set => Payload = (IEnumerable<T>)value; }
+    public IEnumerable<T> Payload { get; set; } = [];
+    object? IQueryResult.Payload { get => Payload; set => Payload = value as IEnumerable<T> ?? []; }
 }
 
 [IgnoreAutoInject]
 public static class QueryResult
 {
+    public static IQueryResult Success(string msg = "操作成功")
+    {
+        return Success<bool>(msg);
+    }
+
+    public static IQueryResult Fail(string msg = "操作失败")
+    {
+        return Fail<bool>(msg);
+    }
+
     public static IQueryResult<T> Success<T>(string msg = "操作成功")
     {
         return new QueryResult<T>()
@@ -121,6 +149,22 @@ public static class TypedResultExtensionForQueryResult
         self.Message = message;
         return self;
     }
+
+    public static IDataTableResult Result(this DataTable payload, bool? success = null)
+    {
+        var s = success ?? payload != null;
+        return new DataTableResult
+        {
+            Success = s,
+            Payload = payload
+        };
+    }
+
+    public static IDataTableResult SetMessage<T>(this IDataTableResult self, string message)
+    {
+        self.Message = message;
+        return self;
+    }
 }
 
 public static class EnumerableExtensionForQueryResult
@@ -129,6 +173,10 @@ public static class EnumerableExtensionForQueryResult
     {
         if (total == 0) total = values.Count();
         return QueryResult.Success<T>().CollectionResult(values, total);
+    }
+    public static IQueryCollectionResult<T> CollectionResult<T>(this IEnumerable<T> values, long total)
+    {
+        return CollectionResult(values, (int)total);
     }
     public static IQueryCollectionResult<T> SetMessage<T>(this IQueryCollectionResult<T> self, string message)
     {
