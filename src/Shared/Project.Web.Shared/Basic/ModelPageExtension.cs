1b9102f0f3805f4c8fc31d39fe645150f61be1f7
MarvelTiter_yaoqinglin <outlinmt@outlook.com>
2024-05-07 21:57:31 +0800

    添加扩展方法

--- src/Shared/Project.Web.Shared/Basic/ModelPageExtension.cs
+++ src/Shared/Project.Web.Shared/Basic/ModelPageExtension.cs
@@ -1,5 +1,6 @@
 using Project.Constraints.Models.Request;
 using Project.Constraints.UI.Extensions;
+using Project.Constraints.UI.Flyout;
 
 namespace Project.Web.Shared.Basic;
 
@@ -13,11 +14,27 @@ public static class ModelPageExtension
         return n;
     }
 
+    public static async Task<TModel> ShowEditFormAsync<TModel, TQuery>(this ModelPage<TModel, TQuery> page, TModel? data, Action<FlyoutOptions<TModel>> config)
+        where TQuery : IRequest, new()
+        where TModel : class, new()
+    {
+        var n = await page.UI.ShowFormDialogAsync(data, page.Options.Columns, true, config);
+        return n;
+    }
+
     public static async Task<TModel> ShowAddFormAsync<TModel, TQuery>(this ModelPage<TModel, TQuery> page, string title, string? width = null)
         where TQuery : IRequest, new()
         where TModel : class, new()
     {
         var n = await page.UI.ShowFormDialogAsync<TModel>(title, null, page.Options.Columns, false, width);
+        return n;
+    }
+
+    public static async Task<TModel> ShowAddFormAsync<TModel, TQuery>(this ModelPage<TModel, TQuery> page, Action<FlyoutOptions<TModel>> config)
+        where TQuery : IRequest, new()
+        where TModel : class, new()
+    {
+        var n = await page.UI.ShowFormDialogAsync(null, page.Options.Columns, false, config);
         return n;
     }
 }
