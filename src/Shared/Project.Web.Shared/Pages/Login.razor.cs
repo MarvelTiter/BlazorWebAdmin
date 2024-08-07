a4a368ccff4bf13a046a3b201c74826055d7a31d
MarvelTiter_yaoqinglin <outlinmt@outlook.com>
2024-08-07 17:50:10 +0800

    备份

--- src/Shared/Project.Web.Shared/Pages/Login.razor.cs
+++ src/Shared/Project.Web.Shared/Pages/Login.razor.cs
@@ -1,81 +1,78 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Web;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.Extensions.Logging;
 using Project.Constraints.UI;
 using Project.Constraints.UI.Extensions;
 using Project.Constraints.Utils;
-using System.Web;
 
-namespace Project.Web.Shared.Pages
+namespace Project.Web.Shared.Pages;
+
+public partial class Login : BasicComponent
 {
-    public partial class Login : BasicComponent
+    private readonly LoginFormModel model = new();
+    [Inject] [NotNull] public IAuthenticationService? AuthService { get; set; }
+    [Inject] [NotNull] private IStringLocalizer<Login>? Localizer { get; set; }
+    [Inject] [NotNull] private IProjectSettingService? CustomSetting { get; set; }
+    [Inject] [NotNull] private ILogger<Login>? Logger { get; set; }
+    [CascadingParameter] [NotNull] private IDomEventHandler? Root { get; set; }
+    public bool Loading { get; set; }
+    public string? Redirect { get; set; }
+
+    protected override void OnInitialized()
     {
-        private LoginFormModel model = new LoginFormModel();
-        [Inject, NotNull] public IAuthenticationService? AuthService { get; set; }
-        [Inject, NotNull] IStringLocalizer<Login>? Localizer { get; set; }
-        [Inject, NotNull] IProjectSettingService? CustomSetting { get; set; }
-        [Inject, NotNull] ILogger<Login>? Logger { get; set; }
-        [CascadingParameter, NotNull] IDomEventHandler? Root { get; set; }
-        public bool Loading { get; set; } = false;
-        public string? Redirect { get; set; }
-        protected override void OnInitialized()
+        base.OnInitialized();
+        var full = Navigator.ToAbsoluteUri(Navigator.Uri);
+        Redirect = HttpUtility.ParseQueryString(full.Query).Get(nameof(Redirect));
+        Root.OnKeyDown += OnPressEnter;
+    }
+
+    private async Task OnPressEnter(KeyboardEventArgs e)
+    {
+        //Console.WriteLine("OnPressEnter: " + e.Key);
+        if (e.Key == "Enter")
         {
-            base.OnInitialized();
-            var full = Navigator.ToAbsoluteUri(Navigator.Uri);
-            Redirect = HttpUtility.ParseQueryString(full.Query).Get(nameof(Redirect));
-            Root.OnKeyDown += OnPressEnter;
+            if (Loading) return;
+
+            await HandleLogin();
         }
+    }
 
-        private async Task OnPressEnter(KeyboardEventArgs e)
+    private async Task HandleLogin()
+    {
+        //Loading = true;
+        BooleanStatusManager.New(b => Loading = b, callback: () => InvokeAsync(StateHasChanged));
+        //await InvokeAsync(StateHasChanged);
+        var result = await AuthService.SignInAsync(model);
+        if (result.Success)
         {
-            //Console.WriteLine("OnPressEnter: " + e.Key);
-            if (e.Key == "Enter")
+            await User.SetUserAsync(result.Payload);
+            UI.Success(Localizer["Login.SuccessTips"].Value);
+            Root.OnKeyDown -= OnPressEnter;
+            var goon = await CustomSetting.LoginInterceptorAsync(result.Payload!);
+            if (goon.Success)
             {
-                if (Loading)
+                await Router.InitRoutersAsync(result.Payload);
+                await AuthenticationStateProvider.IdentifyUser(result.Payload!);
+                if (string.IsNullOrEmpty(Redirect))
                 {
-                    return;
-                }
-                await HandleLogin();
-            }
-        }
-
-        private async Task HandleLogin()
-        {
-            //Loading = true;
-            BooleanStatusManager.New(b => Loading = b, callback: () => InvokeAsync(StateHasChanged));
-            //await InvokeAsync(StateHasChanged);
-            var result = await AuthService.SignInAsync(model);
-            if (result.Success)
-            {
-                await User.SetUserAsync(result.Payload);
-                UI.Success(Localizer["Login.SuccessTips"].Value);
-                Root.OnKeyDown -= OnPressEnter;
-                var goon = await CustomSetting.LoginInterceptorAsync(result.Payload!);
-                if (goon.Success)
-                {
-                    await Router.InitRoutersAsync(result.Payload);
-                    await AuthenticationStateProvider.IdentifyUser(result.Payload!);
-                    if (string.IsNullOrEmpty(Redirect))
-                    {
-                        Logger.LogInformation("Redirect to /");
-                        Navigator.NavigateTo("/");
-                    }
-                    else
-                    {
-
-                        Navigator.NavigateTo(Redirect);
-                    }
+                    Logger.LogInformation("Redirect to /");
+                    Navigator.NavigateTo("/");
                 }
                 else
                 {
-                    UI.Error(goon.Message!);
+                    Navigator.NavigateTo(Redirect);
                 }
             }
             else
             {
-                UI.Error(result.Message!);
+                UI.Error(goon.Message!);
             }
-            //StateHasChanged();
         }
+        else
+        {
+            UI.Error(result.Message!);
+        }
+        //StateHasChanged();
     }
 }
