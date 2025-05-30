5c43dd07062bbbb32a40ed4756f46c99ed8e681e
MarvelTiter_yaoqinglin <outlinmt@outlook.com>
2025-05-30 09:06:01 +0800

    备份，待完成

--- src/Shared/Project.Web.Shared/Basic/ModelPage.cs
+++ src/Shared/Project.Web.Shared/Basic/ModelPage.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using AutoPageStateContainerGenerator;
 using LightExcel;
 using Microsoft.AspNetCore.Components.Rendering;
 using Project.Constraints.Store.Models;
@@ -16,7 +17,9 @@ public abstract class ModelPage<TModel, TQuery> : JsComponentBase
     [Inject][NotNull] private ILogger<ModelPage<TModel, TQuery>>? Logger { get; set; }
     [CascadingParameter] private IAppDomEventHandler? DomEvent { get; set; }
     [CascadingParameter] private TagRoute? RouteInfo { get; set; }
-    public TableOptions<TModel, TQuery> Options { get; set; } = new();
+
+    [SaveState]
+    public virtual TableOptions<TModel, TQuery> Options { get; set; } = new();
     protected bool HideDefaultTableHeader { get; set; }
 
     protected RenderFragment TableFragment => builder =>
