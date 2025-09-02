8671bc8612b385022d349275a5924b4ec4073d4d
MarvelTiter <outlinmt@outlook.com>
2025-09-02 14:51:44 +0800

    小优化

--- src/Shared/Project.Web.Shared/Auth/DynamicPermissionHandler.cs
+++ src/Shared/Project.Web.Shared/Auth/DynamicPermissionHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Project.Web.Shared.Auth;
 public class DynamicPermissionRequirement : IAuthorizationRequirement
@@ -8,7 +9,7 @@ public class DynamicPermissionRequirement : IAuthorizationRequirement
 [AutoInject(ServiceType = typeof(IAuthorizationHandler))]
 public class DynamicPermissionHandler(IUserStore userStore
     , IAppSession session
-    , IPermissionService permissionService) : AuthorizationHandler<DynamicPermissionRequirement>
+    , IServiceProvider serviceProvider) : AuthorizationHandler<DynamicPermissionRequirement>
 {
     private readonly SemaphoreSlim locker = new(1, 1);
     protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, DynamicPermissionRequirement requirement)
@@ -32,6 +33,11 @@ public class DynamicPermissionHandler(IUserStore userStore
             {
                 if (userStore.UserInfo.Permissions is null)
                 {
+                    var permissionService = serviceProvider.GetService<IPermissionService>();
+                    if (permissionService is null)
+                    {
+                        return;
+                    }
                     var permissions = await permissionService.GetUserPermissionsAsync(userStore.UserInfo.UserId);
                     userStore.UserInfo.Permissions = [.. permissions.Payload.Select(p => p.PermissionId)];
                 }
