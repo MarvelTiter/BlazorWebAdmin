4e4f2159d85b887f8bcb51d4acda2e4f84e2e396
MarvelTiter <outlinmt@outlook.com>
2026-03-27 14:08:49 +0800

    备份代码

--- src/Shared/Project.Constraints/UI/Form/FormOptions.cs
+++ src/Shared/Project.Constraints/UI/Form/FormOptions.cs
@@ -4,38 +4,41 @@ namespace Project.Constraints.UI.Form;
 
 public class FormOptions<TData>(IUIService ui, TData? data, List<ColumnInfo> columns) where TData : class, new()
 {
+    public string? FormName { get; set; }
     public IUIService UI { get; } = ui;
     public TData Data { get; } = data ?? new TData();
     public ColumnInfo[] Columns { get; } = [.. columns];
     public int LabelSpan { get; set; } = 6;
     public int WrapperSpan { get; set; }
+    public int? RowCapacity { get; set; }
     public Func<TData, Task<bool>>? OnPostAsync { get; set; }
     public Func<bool>? Validate { get; set; }
     public Action? Update { get; set; }
-    public IEnumerable<ColumnInfo[]> GetRows()
+    public IReadOnlyList<ColumnInfo[]> GetRows()
     {
-        var rowIndex = 1;
-        var settedRow = Columns.Where(c => c.Row.HasValue && c.Row > 0).GroupBy(c => c.Row!.Value).ToDictionary(i => i.Key, i => i.ToArray());
+        var settedRow = Columns.Where(c => c.Row.HasValue && c.Row > 0).GroupBy(c => c.Row!.Value).OrderBy(c => c.Key).ToList();
         var defaultRows = Columns.Where(c => !c.Row.HasValue || c.Row == 0 && c.ShowOnForm);
         var rowEnumerator = defaultRows.GetEnumerator();
-        while (true)
-        {
-            if (settedRow.TryGetValue(rowIndex, out ColumnInfo[]? values))
-            {
-                yield return values;
-            }
-            else
-            {
-                if (rowEnumerator.MoveNext())
-                {
-                    yield return [rowEnumerator.Current];
-                }
-                else
-                {
-                    break;
-                }
-            }
-            rowIndex++;
-        }
+        var list = new List<ColumnInfo[]>();
+        //while (true)
+        //{
+        //    if (settedRow.TryGetValue(rowIndex, out ColumnInfo[]? values))
+        //    {
+        //        yield return values;
+        //    }
+        //    else
+        //    {
+        //        if (rowEnumerator.MoveNext())
+        //        {
+        //            yield return [rowEnumerator.Current];
+        //        }
+        //        else
+        //        {
+        //            break;
+        //        }
+        //    }
+        //    rowIndex++;
+        //}
+        return list;
     }
 }
---
+++ src/Shared/Project.Constraints/UI/Form/IFormBuilder.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Options;
+using Project.Constraints.Common;
+using Project.Constraints.UI.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Constraints.UI.Form;
+
+public interface IFormBuilder<TData>
+{
+    //IFormBuilder<TData> Text<TField>(string label, Expression<Func<TData, TField>> fieldSelector);
+    //IFormBuilder<TData> Number<TField>(string label, Expression<Func<TData, TField>> fieldSelector);
+    //IFormBuilder<TData> Select<TField>(string label, Expression<Func<TData, TField>> fieldSelector, IEnumerable<TField> sources);
+    //IFormBuilder<TData> DateTime<TField>(string label, Expression<Func<TData, TField>> fieldSelector);
+    IFormBuilder<TData> AddField<TField>(string label
+        , Expression<Func<TData, TField>> fieldSelector
+        , int? row = null
+        , Dictionary<string, string>? selectSource = null);
+    RenderFragment Render();
+}
+
+public sealed class FluentFormBuilder<TData>(IUIService ui, TData data, string? formName)
+    : IFormBuilder<TData>
+    where TData : class, new()
+{
+    //public IFormBuilder<TData> DateTime<TField>(string label, Expression<Func<TData, TField>> fieldSelector)
+    //{
+    //    //fieldSelector.ExtractProperty
+    //    return this;
+    //}
+
+    //public IFormBuilder<TData> Number<TField>(string label, Expression<Func<TData, TField>> fieldSelector)
+    //{
+    //    return this;
+    //}
+
+    //public IFormBuilder<TData> Select<TField>(string label, Expression<Func<TData, TField>> fieldSelector, IEnumerable<TField> sources)
+    //{
+    //    return this;
+    //}
+
+    //public IFormBuilder<TData> Text<TField>(string label, Expression<Func<TData, TField>> fieldSelector)
+    //{
+    //    return this;
+    //}
+    private readonly AutoFormBuilder builder = AutoFormBuilder.Create();
+    private FormOptions<TData>? options;
+    public IFormBuilder<TData> AddField<TField>(string label
+        , Expression<Func<TData, TField>> fieldSelector
+        , int? row = null
+        , Dictionary<string, string>? selectSource = null)
+    {
+        var prop = fieldSelector.ExtractProperty();
+        builder.AddField(label, prop, col =>
+        {
+            if (row.HasValue)
+            {
+                col.Row = row.Value;
+            }
+            if (selectSource is not null)
+            {
+                col.EnumValues = selectSource;
+            }
+        });
+        return this;
+    }
+
+    public RenderFragment Render()
+    {
+        options ??= builder.Build(ui, data);
+        options.FormName ??= formName;
+        return ui.BuildForm(options);
+    }
+}
